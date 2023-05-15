29db54107c4baaa26e3365b716085708c62e0385
moonjs0113 <littlemoom@naver.com>
2023-05-15 19:45:50 +0900

    add exit_child

--- lottery.c
+++ lottery.c
@@ -7,8 +7,20 @@ main(int argc, char **argv) {
   int p;
   printf(1, "Lottery Test Start\n");
   p = fork();
-  if (p != 0)
-    print(1, "My Process ID: %d\n", getpid());
+  if (p != 0) // parent
+    printf(1, "My Process ID: %d\n", getpid());
   exit_child(p);
   exit();
+}
+
+void
+exit_child(int pid) {
+  if(pid){
+    if(wait() != pid){
+      printf(1, "wait wrong pid\n");
+      return;
+    }
+  } else {
+    exit();
+  }
 }
