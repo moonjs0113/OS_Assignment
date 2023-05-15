fbd8c57677e2bffa813d5a86a9877d19e47d7c0d
moonjs0113 <littlemoom@naver.com>
2023-05-15 19:13:11 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git pull origin main
+git pull origin Lottery
 make qemu
 qemu-system-i386 -serial mon:stdio -drive file=fs.img,index=1,media=disk,format=raw -drive file=xv6.img,index=0,media=disk,format=raw -smp 2 -m 512 -nographic
