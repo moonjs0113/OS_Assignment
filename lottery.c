091f286f0a38095e48f3984b650bafaef2c81ead
moonjs0113 <littlemoom@naver.com>
2023-05-20 05:59:41 +0900

    Log

--- lottery.c
+++ lottery.c
@@ -6,15 +6,24 @@ void exit_child(int);
   
 int
 main(int argc, char **argv) {
-  int p;
+  int p1, p2;
   printf(1, "Lottery Test Start\n");
   settickets(100);
-  p = fork(); 
-  if (p != 0) {
+  p1 = fork(); 
+  if (p1 != 0) {
     // settickets(rand());
     // printf(1, "Parent - p: %d\n", p);
     printf(1, "Parent Process ID: %d\n", getpid());
     // printf(1, "Parent Process Tickets: %d\n", gettickets());
+    p2 = fork(); 
+    if (p2 != 0) {
+    // settickets(rand());
+    // printf(1, "Parent - p: %d\n", p);
+    printf(1, "Parent Process ID: %d\n", getpid());
+    // printf(1, "Parent Process Tickets: %d\n", gettickets());
+    } else {
+      settickets(100);
+    }
   } else {
     settickets(100);
   }
@@ -24,7 +33,8 @@ main(int argc, char **argv) {
       printf(1, "Parent Value: %d\n", i);
     }
   }
-  exit_child(p);
+  exit_child(p1);
+  exit_child(p2);
   // printf(1, "Rand: %d\n", rand());
   exit();
 }
@@ -32,14 +42,13 @@ main(int argc, char **argv) {
 void
 exit_child(int pid) {
   if(pid){
-    printf(1, "Parent Process Wait Child Process\n");
+    printf(1, "Parent Process Wait Child Process%d\n", pid);
     if(wait() != pid){
       printf(1, "wait wrong pid\n");
       return;
     }
     printf(1, "Parent Process Exit\n");
   } else {
-    
     printf(1, "Child Process ID: %d\n", getpid());
     // printf(1, "Child Process Tickets: %d\n", gettickets());
     printf(1, "Child Process Exit\n");
