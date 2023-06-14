bb312161d585582575b6cf6d768581c276eb79b5
moonjs0113 <littlemoom@naver.com>
2023-06-14 22:02:50 +0900

    Thread_create -ing

--- test.c
+++ test.c
@@ -7,14 +7,14 @@
 
 void *function(void *arg1, void *arg2) {
   printf(1, "Call function\n");
-  // return arg1;
+  return arg1;
 }
 
 int
 main(int argc, char **argv) {
   int a = 1;
   printf(1, "Test Main\n");
-  thread_create(function, (void*)&a, (void*)&a);
+  int i = thread_create(function, (void*)&a, (void*)&a);
   thread_join();
   exit();
 }
