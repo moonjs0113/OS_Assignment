0df61cc77c04d8a42543f41546c9c4e86394544c
moonjs0113 <littlemoom@naver.com>
2023-06-14 20:19:18 +0900

    Create System Call

--- test.c
+++ test.c
@@ -5,10 +5,14 @@
 // int clone(void*, void*, void*, void*);
 // int join(void**);
 
+void function(void *arg) {
+  printf(1, "Call function\n");
+}
+
 int
 main(int argc, char **argv) {
   printf(1, "hello\n");
-  clone();
-  join();
+  clone(NULL, NULL, NULL, NULL);
+  join(NULL);
   exit();
 }
