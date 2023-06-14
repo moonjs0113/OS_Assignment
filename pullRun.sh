f0d28169041f139ffbe19c8d26d5de558f24d5c8
moonjs0113 <littlemoom@naver.com>
2023-06-14 19:20:51 +0900

    Add Script

--- pullRun.sh
+++ pullRun.sh
@@ -1,4 +1,4 @@
 clear
-git pull origin Lottery
+git pull origin Assign2
 make qemu
 qemu-system-i386 -serial mon:stdio -drive file=fs.img,index=1,media=disk,format=raw -drive file=xv6.img,index=0,media=disk,format=raw -smp 2 -m 512 -nographic
