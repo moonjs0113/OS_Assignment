f7aa3e83424dc0093464476d559e709000dc2f57
moonjs0113 <littlemoom@naver.com>
2023-06-14 21:04:02 +0900

    Create Thread

--- thread.c
+++ thread.c
@@ -4,9 +4,11 @@
 int thread_create(void *fnc, void *arg1, void *arg2) {
   int a = 0;
   clone(fnc, arg1, arg2, (void*)&a);
+  return 0;
 }
 
 int thread_join() {
   int a = 0;
   join((void*)&a);
+  return 0;
 }
